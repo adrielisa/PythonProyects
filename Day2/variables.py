9fd25b703bd7a60035af2a2a6043a5b6871eb8f0
Adriel Rodriguez <danielganer67@gmail.com>
2024-12-11 09:55:05 -0500

    Día 1 y 2
    Proyectos

---
+++ Day2/variables.py
@@ -0,0 +1,8 @@
+nombre = input("Dime tu nombre: ")
+print("Tu nombre es " + nombre)
+
+palabra = "Hola "
+palabra2 = "me llamo "
+
+frase = palabra + palabra2 + nombre
+print(frase)
---
+++ Proyect1.py
@@ -0,0 +1,5 @@
+print("Welcome! I'm gonna help you to get a cat name")
+name1 = input("Write the name of your favourite food: ")
+name2 = input("Write one of your lastnames: ")
+
+print("Your cat's name will be: " + name1 + " " + name2)
