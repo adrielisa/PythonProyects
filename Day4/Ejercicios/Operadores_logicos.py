96e5a5561569a26fb77c03f1672a6d29e5965480
Adriel Rodriguez <danielganer67@gmail.com>
2024-12-17 00:01:10 -0500

    Operadores lógicos
    Ejercicios y practicas usando operadores lógicos

---
+++ Day4/Ejercicios/Operadores_logicos.py
@@ -0,0 +1,16 @@
+mi_bool = 4 < 5 and 5 > 6
+print(mi_bool)
+
+mi_bool2 = (55 == 55) and ('perro' == 'perro')
+print(mi_bool2)
+
+mi_bool3 = 10 ==10 or 3 == 30
+print(mi_bool3)
+
+texto = 'esta frase es breve'
+
+mi_bool4 = 'frase' in texto and 'breve' in texto
+print(mi_bool4)
+
+mi_bool5 = not 'a' == 'a'
+print(mi_bool5)
