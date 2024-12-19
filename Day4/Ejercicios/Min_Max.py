8d3d825db5a2f32ba70b36b35966856aca00057e
Adriel Rodriguez <danielganer67@gmail.com>
2024-12-19 12:51:53 -0500

    Min_Max
    Ejercicios y prácticas usando min y max

---
+++ Day4/Ejercicios/Min_Max.py
@@ -0,0 +1,14 @@
+menor = min(56,69,32,2)
+print(menor)
+
+lista = [13,45,56,1]
+print(f'El menor es {min(lista)}')
+
+nombres = ['Juan', 'Pablo', 'Brissa', 'Adriel']
+print(min(nombres))
+
+dic = {'C1':'45', 'C2':'11'}
+
+print(min(dic))
+
+print(min(dic.values()))
