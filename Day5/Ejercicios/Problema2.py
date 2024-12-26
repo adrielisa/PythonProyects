996f49d1d2080914f37159b2123447012a810edc
Adriel Rodriguez <danielganer67@gmail.com>
2024-12-26 02:55:17 -0500

    Problema 2
    Problema realizado aplicando lo aprendido

---
+++ Day5/Ejercicios/Problema2.py
@@ -0,0 +1,27 @@
+"""
+Escribe una función (puedes ponerle cualquier nombre que
+quieras) que reciba cualquier palabra como parámetro, y que
+devuelva todas sus letras únicas (sin repetir) pero en orden
+alfabético.
+Por ejemplo si al invocar esta función pasamos la palabra
+"entretenido"
+, debería devolver ['d'
+,
+'e'
+,
+'i'
+,
+'n'
+,
+'o'
+,
+'r'
+,
+'t']
+"""
+
+def palabra_alfabetica(palabra):
+    
+    return print(sorted(set((palabra))))
+
+palabra_alfabetica('entretenido')
